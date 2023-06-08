dd694f17daedb9dddf00bae00f199e5cab52a6c1
amaralc <calil.amaral@dynamox.net>
2023-06-08 12:21:16 -0300

    chore: cleanup

--- apps/iac/main.tf
+++ apps/iac/main.tf
@@ -3,42 +3,42 @@ provider "vercel" {
   api_token = var.vercel_api_token
 }
 
-resource "vercel_project" "nextjs-app" {
-  name      = "nextjs-app"
-  framework = "nextjs"
-  git_repository = {
-    type = "github"
-    repo = "amaralc/nx-next-terraform-vercel"
-    production_branch = "main"
-  }
+# resource "vercel_project" "nextjs-app" {
+#   name      = "nextjs-app"
+#   framework = "nextjs"
+#   git_repository = {
+#     type = "github"
+#     repo = "amaralc/nx-next-terraform-vercel"
+#     production_branch = "main"
+#   }
 
 
-  build_command = "npx nx build nextjs-app --prod"
-  output_directory = "dist/apps/nextjs-app/.next"
-  dev_command = "npx nx serve nextjs-app"
-}
+#   build_command = "npx nx build nextjs-app --prod"
+#   output_directory = "dist/apps/nextjs-app/.next"
+#   dev_command = "npx nx serve nextjs-app"
+# }
 
-# An environment variable that will be created
-# for this project for the "production" environment.
-resource "vercel_project_environment_variable" "my-env-var-production" {
-  project_id = vercel_project.nextjs-app.id
-  key        = "MY_ENV_VAR"
-  value      = "value-production"
-  target     = ["production"]
-}
+# # An environment variable that will be created
+# # for this project for the "production" environment.
+# resource "vercel_project_environment_variable" "my-env-var-production" {
+#   project_id = vercel_project.nextjs-app.id
+#   key        = "MY_ENV_VAR"
+#   value      = "value-production"
+#   target     = ["production"]
+# }
 
-# An environment variable that will be created
-# for this project for the "production" environment.
-resource "vercel_project_environment_variable" "my-env-var-preview" {
-  project_id = vercel_project.nextjs-app.id
-  key        = "MY_ENV_VAR"
-  value      = "value-preview"
-  target     = ["preview", "development"]
-}
+# # An environment variable that will be created
+# # for this project for the "production" environment.
+# resource "vercel_project_environment_variable" "my-env-var-preview" {
+#   project_id = vercel_project.nextjs-app.id
+#   key        = "MY_ENV_VAR"
+#   value      = "value-preview"
+#   target     = ["preview", "development"]
+# }
 
-resource "vercel_deployment" "nextjs-app-production" {
-  project_id = vercel_project.nextjs-app.id
-  production = true
-  delete_on_destroy = true
-  ref = "main"
-}
+# resource "vercel_deployment" "nextjs-app-production" {
+#   project_id = vercel_project.nextjs-app.id
+#   production = true
+#   delete_on_destroy = true
+#   ref = "main"
+# }
