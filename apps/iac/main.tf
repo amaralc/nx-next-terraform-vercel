680fa811d5265759e936012d9e577bfb2cc5338b
amaralc <calil.amaral@dynamox.net>
2023-06-08 08:32:11 -0300

    feat: add vercel project

--- apps/iac/main.tf
+++ apps/iac/main.tf
@@ -3,11 +3,16 @@ provider "vercel" {
   api_token = var.vercel_api_token
 }
 
-resource "vercel_project" "nx-next-terraform-demo" {
-  name      = "nx-next-terraform-demo"
+resource "vercel_project" "nx-next-terraform-vercel" {
+  name      = "nx-next-terraform-vercel"
   framework = "nextjs"
   git_repository = {
     type = "github"
-    repo = "amaralc/nx-next-terraform-demo"
+    repo = "amaralc/nx-next-terraform-vercel"
+    production_branch = "main"
   }
+
+  build_command = "npx nx build nx-next-terraform-vercel --prod"
+  output_directory = "dist/apps/nx-next-terraform-vercel/.next"
+  dev_command = "npx nx serve nx-next-terraform-vercel"
 }
